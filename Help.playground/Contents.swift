c132f4ef3ef73f20630ac09196f7e4a11c755e31
ellscha <escharlin@wesleyan.edu>
2016-07-06 09:14:30 -0400

    added a function to print a name and an uppercasename from it's return value.

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,14 @@
 let name = "Audrey"
 name.uppercaseString
 
+let myName = "Elli"
+
+let myUppercaseName = myName.uppercaseString
 let uppercaseName = name.uppercaseString
-print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+func wakeUpPrint(name: String, uppercaseName: String) -> String {
+    return ("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+}
+
+print(wakeUpPrint(myName, uppercaseName: myUppercaseName))
+
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
